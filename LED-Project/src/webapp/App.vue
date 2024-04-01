47783335bec5be708231eedbf2241c2285432944
Noah Albers <albers-noah@pm.me>
2024-04-01 13:27:29 +0200

    Setup Project

---
+++ LED-Project/src/webapp/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Runtests v-if="true"/>
+  <v-app v-else>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import Runtests from '@webapp/dev/Runtests.vue';
+</script>
