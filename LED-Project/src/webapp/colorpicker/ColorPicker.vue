ebc017e2dc081a96c38ff340729dc875ec7227fd
Noah Albers <albers-noah@pm.me>
2024-05-10 19:24:41 +0200

    Clearned up the color picker a bit

--- LED-Project/src/webapp/colorpicker/ColorPicker.vue
+++ LED-Project/src/webapp/colorpicker/ColorPicker.vue
@@ -1,48 +1,66 @@
 <template>
-<br>
-    <div class="outer">
+    <br>
+    <div class="frame">
 
-        <div class="display" :style="`background: ${rgbColor}`">
-            <input type="text" :value="rgbColor" :style="textStyle" @change="onInputHexNumber">
+        <!-- HEX-Display (Shows the current color in hex and lets the user edit it)  -->
+        <div class="hex-display"
+            :style="`background: ${hexColor}`">
+            <input type="text"
+                :value="hexColor"
+                :style="hexDisplayTextStyle"
+                @change="onInputHexNumber">
         </div>
-        
-        <div class="split">
-            <div class="panel"
-                ref="refSatureationValueCanvas"
-                @mousedown.left="onSVMouseDown"
-                :style="`background: hsla(${360 * coloredColorType[0]},100%, 50%,1)`">
-                <div class="white-panel"></div>
-                <div class="black-panel"></div>
 
+        <!-- Color picker -->
+        <div class="color-picker">
+
+            <!-- Saturation / Value slider-->
+            <div class="sat-val-slider"
+                ref="refSatValSlider"
+                @mousedown.left="onSatValMouseDown"
+                :style="`background: hsla(${360 * coloredColorType[0]},100%, 50%,1)`">
+
+                <!-- Overlays for saturation and value -->
+                <div class="saturation-panel"></div>
+                <div class="value-panel"></div>
+
+                <!-- Lock bars -->
                 <div class="locked locked-x"
                     :style="cursorSVPosition.top"
                     v-if="locks[2]"></div>
                 <div class="locked locked-y"
                     :style="cursorSVPosition.left"
                     v-if="locks[1]"></div>
+
+                <!-- Cursor-->
                 <div class="cursor inner-cursor"
                     :style="cursorSVPosition.left + cursorSVPosition.top"></div>
             </div>
 
+            <!-- Hue selector -->
             <div class="hue-selector"
                 ref="refHueSlider"
                 @mousedown.left="onHueMouseDown"
                 :style="`background: linear-gradient(0deg,red 0,#ff0 17%,#0f0 33%,#0ff 50%,#00f 67%,#f0f 83%,red)`">
+
+                <!-- Cursor -->
                 <div :class="`cursor hue-cursor ${locks[0] ? 'cursor-disabled' : ''}`"
                     :style="cursorHuePosition"></div>
 
             </div>
         </div>
 
-
+        <!-- Text-controlls -->
         <div class="controlls">
 
-            <div class="line" v-for="_, idx of model">
+            <div class="controller-line"
+                v-for="_, idx of model">
                 <span>{{ propertyNames[idx] }}</span>
 
-                <v-icon @click="triggerSwitch(idx)" :icon="locks[idx] ? 'mdi-lock-outline' : 'mdi-lock-open-outline'"/>
+                <v-icon @click="onLockClicked(idx)"
+                    :icon="locks[idx] ? 'mdi-lock-outline' : 'mdi-lock-open-outline'" />
 
-                <input @input="input($event, idx)"
+                <input @input="onTextInput($event, idx)"
                     :type="locks[idx] ? 'text' : 'number'"
                     min="0"
                     max="1"
@@ -59,11 +77,10 @@
 
 
 <style lang="scss">
-
 $huewidth: 2rem;
 
-$splitwidth: 40rem;
-$splitheight: 20rem;
+$color-picker-width: 40rem;
+$color-picker-height: 20rem;
 
 $border-radius: 3px;
 $spacing: .7rem;
@@ -73,7 +90,7 @@ $text-size: 1.5rem;
 .controlls {
     font-size: $text-size;
 
-    .line {
+    .controller-line {
         display: flex;
         margin-top: $spacing;
 
@@ -89,7 +106,7 @@ $text-size: 1.5rem;
             font-weight: bold;
             border-radius: $border-radius;
 
-            &[type="number"]{
+            &[type="number"] {
                 color: #9CDCFE;
                 font-weight: normal;
             }
@@ -98,10 +115,11 @@ $text-size: 1.5rem;
 
 }
 
-.display {
+.hex-display {
     border-radius: $border-radius;
 
-    &,input {   
+    &,
+    input {
         width: 100%;
         font-size: $text-size;
     }
@@ -131,17 +149,17 @@ $text-size: 1.5rem;
     }
 }
 
-.split {
+.color-picker {
     display: inline-flex;
     align-items: stretch;
     justify-content: center;
     margin-top: $spacing;
-    
-    width: $splitwidth;
-    height: $splitheight;
+
+    width: $color-picker-width;
+    height: $color-picker-height;
 }
 
-.outer {
+.frame {
     color: rgb(232, 232, 232);
     display: inline-flex;
     flex-direction: column;
@@ -149,7 +167,7 @@ $text-size: 1.5rem;
     padding: $spacing;
 }
 
-.panel {
+.sat-val-slider {
     border-radius: $border-radius;
     overflow: hidden;
     flex-grow: 1;
@@ -187,14 +205,15 @@ $text-size: 1.5rem;
             transform: translateX(-50%);
         }
     }
-}
 
-.black-panel {
-    background: linear-gradient(0deg, #000, transparent);
-}
 
-.white-panel {
-    background: linear-gradient(90deg, #fff, hsla(0, 0%, 100%, 0))
+    .value-panel {
+        background: linear-gradient(0deg, #000, transparent);
+    }
+
+    .saturation-panel {
+        background: linear-gradient(90deg, #fff, hsla(0, 0%, 100%, 0))
+    }
 }
 
 .cursor {
@@ -227,60 +246,123 @@ $text-size: 1.5rem;
         (e: "preview", hex: string): void
     }>();
 
-    const refSatureationValueCanvas = ref(null);
+    const refSatValSlider = ref(null);
     const refHueSlider = ref(null);
+
     const isHueMouseDown = useMouse(onHueSliderMove);
-    const isSatValMouseDown = useMouse(onSatValueSliderMove);
+    const isSatValMouseDown = useMouse(onSatValSliderMove);
 
     const propertyNames = ["Hue", "Saturation", "Value"];
 
-    const textStyle = computed(()=>{
-        if(coloredColorType.value[2] >= 1/2)
+    // Computed a style for the text-display
+    const hexDisplayTextStyle = computed(() => {
+        if (coloredColorType.value[2] >= 1 / 2)
             return "color: black; text-shadow: 1px 1px #aaa;";
-        
+
         return "color: white; text-shadow: 1px 1px #444;";
     })
 
+    // V-Model of this component
     const model = defineModel({
         required: true,
         type: Object as PropType<VariableColorType>
     })
 
+    // Store for variables
     const varStore = useVariableStore();
 
+    //#region Computed
+
+    // Computed: Which of the items (HSV) have been locked to be string expressions
+    // and which can therefor not be altered by the color picker interface
     const locks = computed(() => model.value.map(x => typeof x === "string"));
 
-    function onSVMouseDown(evt: any){
-        isSatValMouseDown.value = true;
-        onSatValueSliderMove(evt);
-    }
-    
-    function onHueMouseDown(evt: any){
-        isHueMouseDown.value = true;
-        onHueSliderMove(evt);
-    }
 
-    function onInputHexNumber(evt: any){
-        const clr = HEX2HSV(evt.target.value);
+    // Computed: Css-Style for the hue cursor position
+    const cursorHuePosition = computed(() => {
+        return `top: ${coloredColorType.value[0] * 100}%;`;
+    });
 
-        if(clr === false) return;
+    // Computed: Css-Style for the saturation / value cursor position
+    const cursorSVPosition = computed(() => {
+        const top = (1 - coloredColorType.value[2]) * 100;
+        const left = (coloredColorType.value[1]) * 100;
 
-        model.value = [clr.h, clr.s, clr.v];
-    }
+        return {
+            top: `top: ${top}%;`,
+            left: `left: ${left}%;`
+        }
+    })
 
-    function updateValue(idx: number, value: number | string) {
+    // Computed: The HSV component's all as numbers (Expressions have been calculated)
+    const coloredColorType = computed(() => {
+
+        // Maps each value to their calculated value
+        return model.value.map(itm => {
+            if (typeof itm === "number") return itm;
+
+            return clamp(solveExpression(itm, varStore.variable2ValueMap, 1));
+        })
+    })
+
+    // Computed: The HSV componenets as a HEX-Css color #FFFFFF for example
+    const hexColor = computed(() => {
+        const asString = HSV2HEX(
+            coloredColorType.value[0] as number,
+            coloredColorType.value[1] as number,
+            coloredColorType.value[2] as number
+            , false);
+
+        emit("preview", asString);
+
+        return asString;
+    });
+
+    //#endregion
+
+    // Simple clamp method
+    const clamp = (x: number) => Math.min(Math.max(x, 0), 1);
+    // Simple round method
+    const roundNumber = (value: number) => Math.round(clamp(value) * 1000) / 1000;
+
+    // Sets the model value of one of hue / saturation / value by index
+    function setModelValue(idx: number, value: number | string) {
 
         const updated = model.value;
-        updated[idx] = typeof value === "string" ? value : roundNum(value);
+        updated[idx] = typeof value === "string" ? value : roundNumber(value);
 
         model.value = updated
     }
 
-    function input(evt: any, idx: number) {
+    // Event: When the user clicks the saturation / value curser
+    function onSatValMouseDown(evt: any) {
+        isSatValMouseDown.value = true;
+        onSatValSliderMove(evt);
+    }
+
+    // Event: When the user clicks the hue curser
+    function onHueMouseDown(evt: any) {
+        isHueMouseDown.value = true;
+        onHueSliderMove(evt);
+    }
+
+    // Event: When the user inputs a custom hex number
+    function onInputHexNumber(evt: any) {
+        const clr = HEX2HSV(evt.target.value);
+
+        // Validates the result
+        if (clr === false) return;
+
+        model.value = [clr.h, clr.s, clr.v];
+    }
+
+
+    // Event: When one of the textboxes is used to input a number for one of the components
+    function onTextInput(evt: any, idx: number) {
         const elm = evt.target;
 
         if (locks.value[idx]) {
-            updateValue(idx, elm.value);
+            setModelValue(idx, elm.value);
             return;
         }
 
@@ -294,22 +376,18 @@ $text-size: 1.5rem;
             return;
         }
 
-        updateValue(idx, v);
+        setModelValue(idx, v);
     }
 
-    function triggerSwitch(idx: number) {
+    // Event: When the padlock is clicked to lock or unlock a component
+    function onLockClicked(idx: number) {
 
         const isString = typeof model.value[idx] === "string";
 
-        updateValue(idx, isString ? coloredColorType.value[idx] : model.value[idx].toString());
+        setModelValue(idx, isString ? coloredColorType.value[idx] : model.value[idx].toString());
     }
 
-    const clamp = (x: number)=>Math.min(Math.max(x,0),1);
-
-    function roundNum(value: number) {
-        return Math.round(clamp(value) * 1000) / 1000;
-    }
-
+    // Event: When the user moves the hue curser
     function onHueSliderMove(evt: any) {
         if (locks.value[0]) return;
 
@@ -318,11 +396,12 @@ $text-size: 1.5rem;
         const bcr = src.getBoundingClientRect();
 
         const clickY = (evt.clientY - bcr.top) / bcr.height;
-        updateValue(0, clickY);
+        setModelValue(0, clickY);
     }
 
-    function onSatValueSliderMove(evt: any) {
-        const src = refSatureationValueCanvas.value as any as HTMLElement;
+    // Event: When the user moves the saturation / value curser
+    function onSatValSliderMove(evt: any) {
+        const src = refSatValSlider.value as any as HTMLElement;
 
         const bcr = src.getBoundingClientRect();
 
@@ -331,55 +410,19 @@ $text-size: 1.5rem;
 
         if (!locks.value[1]) {
             const clickX = (evt.clientX - bcr.left) / bcr.width;
-            updated[1] = roundNum(clickX);
+            updated[1] = roundNumber(clickX);
             changeFlag = true;
         }
 
         if (!locks.value[2]) {
             const clickY = (evt.clientY - bcr.top) / bcr.height;
-            updated[2] = roundNum(1 - clickY);
+            updated[2] = roundNumber(1 - clickY);
             changeFlag = true;
         }
 
         if (changeFlag)
             model.value = updated;
     }
-
-    const cursorHuePosition = computed(() => {
-        return `top: ${clamp(coloredColorType.value[0]) * 100}%;`;
-    });
-
-    const cursorSVPosition = computed(() => {
-        const top = (clamp(1 - coloredColorType.value[2])) * 100;
-        const left = (clamp(coloredColorType.value[1])) * 100;
-
-        return {
-            top: `top: ${top}%;`,
-            left: `left: ${left}%;`
-        }
-    })
-
-    const coloredColorType = computed(() => {
-
-        // Maps each value to their calculated value
-        return model.value.map(itm => {
-            if (typeof itm === "number") return itm;
-
-            return solveExpression(itm, varStore.variable2ValueMap, 1);
-        })
-    })
-
-    const rgbColor = computed(() => {
-        const asString = HSV2HEX(
-            coloredColorType.value[0] as number,
-            coloredColorType.value[1] as number,
-            coloredColorType.value[2] as number
-            , false);
-
-        emit("preview", asString);
-
-        return asString;
-    });
 
 
 
